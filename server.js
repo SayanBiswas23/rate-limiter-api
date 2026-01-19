b6b8b97a5ba6910f98d0dd4576463e967b542ec7
SayanBiswas23 <bsayan238@gmail.com>
2026-01-19 22:03:55 +0530

    try catch wrap connection db

--- server.js
+++ server.js
@@ -3,17 +3,24 @@ import dotenv from 'dotenv'; // Import dotenv and configure it
 import connectDB from './src/config/db.js'; // Corrected import
 import userroutes from './src/routes/users.routes.js';
 import { ratelimiter } from './src/middleware/ratelimiter.middleware.js';
+dotenv.config({
+});
 
 const app = express();
-dotenv.config();
 // Connect to the database before starting the server
-connectDB();
+try {
+    await connectDB();
+} catch (error) {
+    res.status(500).json({
+        msg: 'Database connection failed;',
+    });
+}
 
 app.use(express.json()); // Corrected middleware usage
-app.use('/api/users', userroutes);
 // !global ratelimiter
 app.use(ratelimiter);
 // !rate limiter for expensive endpoints
+app.use('/api/users', userroutes);
 // //app.use('/api/users', ratelimiter);
 app.get('/', (req, res) => {
     res.send(`API is runnnig `);
